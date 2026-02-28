d137855a8b0fe523757d4a1639e43e416754aeac
Matilde2013 <martini.matilde.2013@gmail.com>
2026-02-28 09:23:12 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+// File: sw.js
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+    event.waitUntil(
+        self.registration.showNotification(data.title, {
+            body: data.body,
+            icon: '/icon.png'
+        })
+    );
+});
